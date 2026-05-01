b08a9d5dbb7bc9d159f7f8cf104fc724ea366941
Agent-smith018 <prajapatismit212@gmail.com>
2026-05-01 13:33:31 -0400

    chore: prepare project for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,13 @@ const exerciseLogRoutes = require("./routes/exerciseLogRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // Routes
